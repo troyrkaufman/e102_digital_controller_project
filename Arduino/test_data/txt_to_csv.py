7c0d80e5a6e65bd69bc9cd2463a16f9f6ed0eee8
Charles Lambert <143451737+clambert2@users.noreply.github.com>
2025-05-05 22:47:37 -0700

    matlab directory and beginging of plotting code

--- Arduino/test_data/txt_to_csv.py
+++ Arduino/test_data/txt_to_csv.py
@@ -9,4 +9,3 @@ print(data)
 file_name = file_name.split('.')[0] + '.csv'
 
 np.savetxt(file_name, data, delimiter=',', header='Time,Value', comments='')
-  
---
+++ Matlab/step_repsonse_plot.m
@@ -0,0 +1,14 @@
+% project_01_script.m
+%
+% Charlie Lambert and Troy Kaufman
+% E102 Spring 2024, Prof. Cha, Tsai, Wu, and Yang
+%
+% Project 2
+
+clear
+clc 
+close all
+
+% Load the data from csv
+data = readtable('step_response_data.csv');
+
