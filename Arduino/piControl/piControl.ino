a8b4127a2a8a816b09fce61ac6d8bdff63fb992c
troyrkaufman <tkaufman@g.hmc.edu>
2025-05-07 15:29:11 -0700

    wip pi control

---
+++ Arduino/piControl/piControl.ino
@@ -0,0 +1,86 @@
+// piControl.ino
+
+
+//PIN SETTINGS
+const int yPin = A0; // Analog read pin
+const int uPin = 9; // Analog write pin (PWM)
+const int switchPin = 2; // input pin for the switch
+boolean switchVal = HIGH; // declare initial switch pin state
+
+// Parameter Settings
+int time = 0; // initialize time
+int uVal = 0; // initialize control input
+const float Kp = 2.3492;
+const float Ki = 0.8551;
+const float ref = 2.5; //2.5V reference input
+
+// Initial Values for internal signals
+float y=0;
+float u=0;
+float e=0;
+float e_integral = 0;
+
+void setup() {
+    pinMode(switchPin, INPUT); //set switch pin to input mode
+    digitalWrite(switchPin,HIGH); //initialize to start with pull up voltage
+    Serial.begin(9600);
+}
+
+void loop() {
+    //WAIT FOR SWITCH
+    while(switchVal != HIGH) // repeat this loop until switch is turned on
+    // (switchVal will go LOW when switch is turned on)
+    {
+        analogWrite(uPin,0);
+        switchVal = digitalRead(switchPin); // read switch state
+    }
+    // READ CIRCUIT OUTPUT
+    int sensorVal = analogRead(yPin);
+    
+    // convert to volts
+    y=sensorVal*(5.0/1023.0);
+
+    // calculate error for p control
+    e=ref-y;
+
+    // calculate error for integral which is approximately the running sum of the error * Ts
+    e_integral += e*0.1;
+
+    // CONTROLLER
+    u=Kp*e + Ki*e_integral;
+
+    // check that control signal is in range
+    if (u>5) {
+        u=5;
+    }
+    if (u<0){
+        u=0;
+    }
+    if (e_integral>5) {
+        e_integral=5;
+    }
+    if (e_integral<0){
+        e_integral=0;
+    }
+
+    // WRITE CIRCUIT INPUT
+    uVal=u*(255/5);
+    analogWrite(uPin,uVal);
+
+    // print the results to the serial monitor:
+    Serial.print(time++);
+    Serial.print (" ");
+    Serial.print (y);
+    Serial.print (" ");
+    Serial.print(u);
+    Serial.print (" ");
+    Serial.print(uVal);
+    Serial.print (" ");
+    Serial.print(e);
+    Serial.print (" ");
+    Serial.println(e_integral);
+
+    // WAIT FOR NEXT SAMPLE
+    delay(100); //sample frequency 10Hz
+    switchVal = digitalRead(switchPin); // read switch state
+}
