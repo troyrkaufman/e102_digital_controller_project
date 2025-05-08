57f8520a600ca79d05803e9b3d06455b9d471abe
Charles Lambert <143451737+clambert2@users.noreply.github.com>
2025-05-07 22:26:46 -0700

    observer gain:

--- Matlab/state_space_derivation.m
+++ Matlab/state_space_derivation.m
@@ -17,7 +17,14 @@ sys_c = ss(A, B, C, D)
 Ts = 0.1
 sys_d = c2d(sys_c, Ts)
 
-% implement controller
+% solve for K and poles
 Q = [100 0; 0 1];
 R = [1];
 [K, S, P] = dlqr(sys_d.A ,sys_d.B, Q, R)
+
+% solve reference gain
+Kr = -inv((sys_c.C-sys_c.D*K)*inv(sys_d.A-eye(2)-sys_d.B*K)*sys_d.B-sys_c.D)
+
+% solve observer gains
+P_o = 2*P;
+L = place(sys_d.A', sys_d.C', P_o)
